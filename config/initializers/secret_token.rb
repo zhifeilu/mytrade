088a46c9ce42f0ba806d1d07605ed8e7527f9cc7
31381242@qq.com <31381242@qq.com>
2013-07-10 11:04:27 +0800

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytrade::Application.config.secret_token = 'f1ec54c9c45aaaa3461e60c97ffc337dcd30980043a4c4af2c81c6a01e3658791c8fca8d8aeb87bd39252d722bf20aaa200aa4d0726f24e85baf21deefacfdc5'
